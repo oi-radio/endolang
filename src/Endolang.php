e11690dbbfad37edbc0fd4b3dd4e1b028812c990
y_ahiru <yahiru1121@gmail.com>
2024-04-16 13:56:31 +0900

    init

---
+++ src/Endolang.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yahiru\Endolang;
+
+use Exception;
+
+final class Endolang
+{
+    public function run(string $code): void
+    {
+        throw new Exception('Not implemented yet');
+    }
+}
